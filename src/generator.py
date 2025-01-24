ab9e6e3dd4118dcb27a25131525877e9b2ffe5ae
PavanCodes05 <pavancodes05@gmail.com>
2025-01-24 20:49:39 +0530

    Complete Bold&Italic Convert

--- src/generator.py
+++ src/generator.py
@@ -7,7 +7,21 @@ def generate_html(contents):
             count = content.count("#")
             value = content[count: ]
 
-            tag = f"<h{count}>{value.strip()}<h{count}>"        
+            tag = f"<h{count}>{value.strip()}</h{count}>"        
+            tags.append(tag)
+
+        # Bold
+        elif content[0] == "*" and content[1] == "*":
+            value = content.strip("**")
+            
+            tag = f"<b>{value}</b>"
+            tags.append(tag)
+
+        # Italic
+        elif content[0] == "_":
+            value = content.strip("_")
+
+            tag = f"<em>{value}</em>"
             tags.append(tag)
 
     return tags
